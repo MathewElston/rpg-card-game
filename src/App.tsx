11cac3ca2d9fa741cb9a9ef2a5053c714baa4513
Mathew Elston <elstonmathew@yahoo.com>
2023-08-05 09:25:14 -0700

    Handle attack function & remove enemy on death

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,73 @@ import { useState } from "react";
 
 import "./App.css";
 import GameCard from "./components/GameCard";
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Button, Grid, GridItem } from "@chakra-ui/react";
+import { Character } from "./Character";
+import AttackMenu from "./components/AttackMenu";
+import GameBoard from "./components/GameBoard";
+import PlayerField from "./components/PlayerField";
 
 function App() {
+  const [player, setPlayer] = useState<Character>({
+    id: 0,
+    image: "./src/assets/dark-knight.webp",
+    name: "Dark Knight",
+    health: 100,
+    mana: 50,
+    attack: 5,
+    type: "Dark",
+  });
+  const [enemyList, setEnemyList] = useState<Character[]>([
+    {
+      id: 1,
+      image: "./src/assets/goblin.webp",
+      name: "GoblinA",
+      attack: 2,
+      health: 100,
+      mana: 50,
+      type: "Rock",
+    },
+    {
+      id: 2,
+      image: "./src/assets/goblin.webp",
+      name: "GoblinB",
+      attack: 2,
+      health: 100,
+      mana: 50,
+      type: "Rock",
+    },
+    {
+      id: 3,
+      image: "./src/assets/goblin.webp",
+      name: "GoblinC",
+      attack: 2,
+      health: 100,
+      mana: 50,
+      type: "Rock",
+    },
+    {
+      id: 4,
+      image: "./src/assets/goblin.webp",
+      name: "GoblinD",
+      attack: 2,
+      health: 100,
+      mana: 50,
+      type: "Rock",
+    },
+    {
+      id: 5,
+      image: "./src/assets/goblin.webp",
+      name: "GoblinE",
+      attack: 2,
+      health: 100,
+      mana: 50,
+      type: "Rock",
+    },
+  ]);
+
   return (
     <>
-      <Grid>
-        <GameCard />
-        <GridItem></GridItem>
-      </Grid>
+      <GameBoard player={player} enemyList={enemyList} />
     </>
   );
 }
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,37 +11,50 @@ import {
   Progress,
   Stack,
   Text,
-  useColorMode,
 } from "@chakra-ui/react";
-import darkKnight from "../assets/dark-knight.webp";
-const GameCard = () => {
+import { useState } from "react";
+import AttackMenu from "./AttackMenu";
+import { Character } from "../Character";
+import AttackButton from "./AttackButton";
+
+interface Props {
+  character: Character;
+  targetList?: Character[];
+  handleAttack: () => void;
+}
+const GameCard = ({ character, handleAttack }: Props) => {
   return (
     <Card textColor="orange.400" height="400px" width="200px" borderRadius={5}>
       <CardBody>
         <Stack spacing={2}>
-        <HStack justifyContent="space-between">
-            <Text>Name</Text>
-            <Text>Type</Text>
+          <HStack justifyContent="space-between">
+            <Text>{character.name}</Text>
+            <Text>{character.type}</Text>
           </HStack>
           <Image
             width="100%"
             overflow="hidden"
-            src={darkKnight}
+            src={character.image}
             borderRadius={20}
           />
           <Divider borderColor="white" />
           <HStack flex="0" justifyContent="space-between">
-            <Text> Health </Text>
-            <Text> Mana </Text>
+            <Text> {character.health} </Text>
+            <Text> {character.mana} </Text>
           </HStack>
           <Grid templateColumns="repeat(2, 1fr)" gap={2}>
-            <Button>Attack</Button>
+            <AttackButton
+              handleAttack={handleAttack}
+            />
             <Button>Abilities</Button>
             <Button>Defend</Button>
             <Button>Item</Button>
           </Grid>
-          <Progress value={50} borderRadius={20}> </Progress>
+          <Progress value={50} borderRadius={20}>
+            {" "}
+          </Progress>
         </Stack>
+
       </CardBody>
     </Card>
   );
